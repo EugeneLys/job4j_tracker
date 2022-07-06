bd11d4135e95cb1326db0cd4f6a3e4002adf3130
EugeneLys <eugene.lysakov@gmail.com>
2022-07-07 01:08:03 +0500

    9. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,8 +8,6 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item first = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String whenFirstCreated = first.getCreated().format(formatter);
-        System.out.println("first создан: " + whenFirstCreated);
+        System.out.println(first);
     }
 }
