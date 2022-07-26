a7920196f06b62fe013c89a343b1779a7e208fe2
Евгений <eugene.lysakov@gmail.com>
2022-07-26 13:32:12 +0500

    7. Шаблон проектирования - Стратегия [#786 #325658]

--- src/main/java/ru/job4j/tracker/DeleteAction.java
+++ src/main/java/ru/job4j/tracker/DeleteAction.java
@@ -7,7 +7,7 @@ public class DeleteAction implements UserAction {
     }
 
     @Override
-    public boolean execute (Input input, Tracker tracker) {
+    public boolean execute(Input input, Tracker tracker) {
         System.out.println("=== Delete item ===");
         int id = input.askInt("Enter id: ");
         if (tracker.delete(id)) {
