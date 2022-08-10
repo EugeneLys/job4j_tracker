a99822fa3d763d1ec234a6377e3733ab1d1a60ee
EugeneLys <eugene.lysakov@gmail.com>
2022-08-10 20:13:52 +0500

    ElementAbuseException("")

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -15,6 +15,31 @@ public class FindEl {
         return rsl;
     }
 
+    public static boolean sent(String value, String[] abuses) throws  ElementAbuseException {
+        for (int index = 0; index < abuses.length; index++) {
+            if (value.equals(abuses[index])) {
+                throw new ElementAbuseException("");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         String[] words = new String[] {"first", "second", "third"};
         try {
