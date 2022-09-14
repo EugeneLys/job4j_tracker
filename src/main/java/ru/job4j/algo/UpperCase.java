484fa2980e4a31c64812fb60ca11e76085523df0
Евгений <eugene.lysakov@gmail.com>
2022-09-14 17:21:57 +0500

    Упражнения

---
+++ src/main/java/ru/job4j/algo/UpperCase.java
@@ -0,0 +1,9 @@
+package ru.job4j.algo;
+
+public class UpperCase {
+    public static void main(String[] args) {
+        int x = 9;
+        System.out.println("Значение х равно " + x);
+        System.out.println("Значение квадрата х равно " + x * x);
+    }
+}
