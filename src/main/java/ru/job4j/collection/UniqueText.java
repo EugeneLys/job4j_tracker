98d5070b108f700ee1d1c6cda6a8259d791f979f
Евгений <eugene.lysakov@gmail.com>
2022-10-12 09:52:07 +0500

    2. Идентичные тексты [#212655 #350875]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 
 public class UniqueText {
     public static boolean isEquals(String originText, String duplicateText) {
-        boolean rsl = true;
         String[] origin = originText.split(" ");
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>();
@@ -13,9 +12,9 @@ public class UniqueText {
         }
         for (String duplicate : text) {
             if (!check.contains(duplicate)) {
-                return !rsl;
+                return false;
             }
         }
-        return rsl;
+        return true;
     }
 }
