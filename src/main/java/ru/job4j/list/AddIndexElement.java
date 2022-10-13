e898ddb199c7ee99cab8adea05c3fa79a89088df
Евгений <eugene.lysakov@gmail.com>
2022-10-13 12:52:34 +0500

    Упражнения

---
+++ src/main/java/ru/job4j/list/AddIndexElement.java
@@ -0,0 +1,15 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddIndexElement {
+    public static boolean addNewElement(List<String> list, int index, String str) {
+        List<String> check = new ArrayList<>(list);
+        if (check.contains(str)) {
+            return false;
+        }
+        check.add(index, str);
+        return check.size() != list.size();
+    }
+}
