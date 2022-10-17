903424daac66a290587c4bcceff2f3130b35752f
Евгений <eugene.lysakov@gmail.com>
2022-10-17 11:20:50 +0500

    2. Преобразования List в Map. [#10093]

---
+++ src/test/java/ru/job4j/collection/OrderConvertTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class OrderConvertTest {
+    @Test
+    public void whenSingleOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        Order expected = new Order("3sfe", "Dress");
+        Assertions.assertEquals(map.get("3sfe"), expected);
+    }
+
+    @Test
+    public void whenDuplicaeOrders() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("3sfe", "Dress"));
+        orders.add(new Order("3sfe", "Shoes"));
+        orders.add(new Order("3sfe", "Phone"));
+        HashMap<String, Order> map = OrderConvert.process(orders);
+        Assertions.assertEquals(map.size(), 1);
+    }
+
+}
