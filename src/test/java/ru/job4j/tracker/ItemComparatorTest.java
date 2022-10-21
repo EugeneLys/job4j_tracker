b76d95384012a2c4a8e8616b916d49e915df1fe8
Евгений <eugene.lysakov@gmail.com>
2022-10-21 11:18:13 +0500

    0. Сортировка [#10096]

--- src/test/java/ru/job4j/tracker/ItemComparatorTest.java
+++ src/test/java/ru/job4j/tracker/ItemComparatorTest.java
@@ -2,28 +2,52 @@ package ru.job4j.tracker;
 
 import java.util.*;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.util.CollectionUtils;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class ItemComparatorTest {
 
     @Test
     public void whenItemAscByName() {
-        List<Item> items = List.of(
-        new Item(1, "one"),
-        new Item(4, "four"),
-        new Item(5, "five")
-        );
-        Collections.sort(items, new ItemAscByName());
-        List<Item> expected = List.of(
-        new Item(5, "five"),
-        new Item(4, "four"),
-        new Item(1, "one")
-        );
-        assertThat(items).hasSameElementsAs(expected);
+        List<Item> items = new ArrayList<>();
+        items.add(0, new Item(2, "bbb"));
+        items.add(1, new Item(3, "ccc"));
+        items.add(2, new Item(1, "aaa"));
+        items.sort(new ItemAscByName());
+        List<Item> expected = new ArrayList<>();
+        expected.add(0, new Item(1, "aaa"));
+        expected.add(1, new Item(2, "bbb"));
+        expected.add(2, new Item(3, "ccc"));
+        ArrayList<Integer> itemsID = new ArrayList<>();
+        for (Item item : items) {
+            itemsID.add(item.getId());
+        }
+        ArrayList<Integer> expectedID = new ArrayList<>();
+        for (Item exp : expected) {
+            expectedID.add(exp.getId());
+        }
+        Assertions.assertEquals(itemsID, expectedID);
+    }
+
+    @Test
+    public void whenItemDescByName() {
+        List<Item> items = new ArrayList<>();
+        items.add(0, new Item(111, "A111"));
+        items.add(1, new Item(222, "B222"));
+        items.add(2, new Item(333, "C333"));
+        items.sort(new ItemDescByName());
+        List<Item> expected = new ArrayList<>();
+        expected.add(0, new Item(333, "C333"));
+        expected.add(1, new Item(222, "B222"));
+        expected.add(2, new Item(111, "A111"));
+        ArrayList<Integer> itemsID = new ArrayList<>();
+        for (Item item : items) {
+            itemsID.add(item.getId());
+        }
+        ArrayList<Integer> expectedID = new ArrayList<>();
+        for (Item exp : expected) {
+            expectedID.add(exp.getId());
+        }
+        Assertions.assertEquals(itemsID, expectedID);
     }
 }
