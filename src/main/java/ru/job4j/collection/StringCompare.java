16ab9f3a93d2c0a580d055e816cc1f68ca371c60
Евгений <eugene.lysakov@gmail.com>
2022-10-25 11:40:05 +0500

    3. Компаратор для строк [#35008 #354504]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,13 +6,13 @@ public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int rsl;
-        for (int i = 0; i < left.length() && i < right.length(); i++) {
+        int size = Math.min(left.length(), right.length());
+        for (int i = 0; i < size; i++) {
             rsl = Character.compare(left.charAt(i), right.charAt(i));
             if (rsl != 0) {
                 return rsl;
             }
         }
-        rsl = Integer.compare(left.length(), right.length());
-        return rsl;
+        return Integer.compare(left.length(), right.length());
     }
 }
