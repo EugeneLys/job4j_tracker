5be9df5d8baf92c433767a17e9acc1faeebd0bb4
Евгений <eugene.lysakov@gmail.com>
2022-11-27 16:40:32 +0500

    Упражнения

--- src/main/java/ru/job4j/map/MostUsedCharacter.java
+++ src/main/java/ru/job4j/map/MostUsedCharacter.java
@@ -4,16 +4,24 @@ import java.util.*;
 
 public class MostUsedCharacter {
     public static char getMaxCount(String str) {
-        str.toLowerCase();
-        char[] array = str.toCharArray();
-        Map<Character, Integer> map = new TreeMap<>();
-        for (Character el : array
-             ) {
-            map.computeIfPresent(el);
-            map.putIfAbsent(el, );
-
+        str = str.replaceAll("\\s+", "");
+        String str2 = str.toLowerCase();
+        char[] array = str2.toCharArray();
+        TreeMap<Character, Integer> map = new TreeMap<>();
+        for (char c : array) {
+            map.computeIfPresent(c,
+                    (key, val) -> val + 1);
+            map.putIfAbsent(c, 1);
         }
+        int x = 1;
         char rsl = ' ';
+        for (Character key : map.keySet()) {
+            int y = map.get(key);
+            if (y > x) {
+                rsl = key;
+                x = y;
+            }
+        }
         return rsl;
     }
 }
