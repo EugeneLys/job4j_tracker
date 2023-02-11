a1ddcadba6405427fe04850050b0d8ebc6bc7090
Евгений <eugene.lysakov@gmail.com>
2023-02-11 16:17:55 +0500

    Exercises

---
+++ src/main/java/ru/job4j/stream/StreamDemo3.java
@@ -0,0 +1,16 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+
+public class StreamDemo3 {
+
+    public static void main(String[] args) {
+        ArrayList<Double> myList = new ArrayList<>();
+        myList.add(4.0);
+        myList.add(9.0);
+        myList.add(16.0);
+
+        double productOfSum = myList.stream().reduce(1.0, (a,b) -> a * Math.sqrt(b), (a,b) -> a * b);
+        System.out.println("result: " + productOfSum);
+    }
+}
